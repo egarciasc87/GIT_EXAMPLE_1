9d1f9b86de2454d290eb22d73a5cf4da23a3a41b
Erick Garcia <egarciasc87@gmail.com>
2022-07-17 16:08:15 +0300

    version 2: add math functions

--- Example1.py
+++ Example1.py
@@ -1,3 +1,15 @@
 import os
 
+def substract(x, y):
+    result = int(x) - int(y)
+    return result
+
+def add(x, y):
+    result = int(x) + int(y)
+    return result
+
 print("Kello world...")
+number1 = input("Enter number 1:")
+number2 = input("Enter number 2:")
+print("Add result is ", add(number1, number2))
+print("Substract result is ", substract(number1, number2))
