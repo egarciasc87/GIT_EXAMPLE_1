4d296a5be32a3e7689a5586762e6e3181b0e57ce
Erick Garcia <egarciasc87@gmail.com>
2022-07-17 16:33:21 +0300

    version 3: add multiply and split

--- Example1.py
+++ Example1.py
@@ -8,6 +8,14 @@ def add(x, y):
     result = int(x) + int(y)
     return result
 
+def multiply(x, y):
+    result = int(x) * int(y)
+    return result
+
+def split(x, y):
+    result = int(x) / int(y)
+    return result
+
 def enterNumber(label):
     res = False
     number = 0
@@ -24,3 +32,5 @@ number1 = enterNumber("Enter number 1:")
 number2 = enterNumber("Enter number 2:")
 print("\nAdd result is ", add(number1, number2))
 print("Substract result is ", substract(number1, number2))
+print("Multiply result is ", multiply(number1, number2))
+print("Split result is ", split(number1, number2))
